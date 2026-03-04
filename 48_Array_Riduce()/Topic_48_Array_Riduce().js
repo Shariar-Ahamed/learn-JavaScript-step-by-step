61fac3f38d87fb0cf3cb0bcd9fa8099eece82097
ShahriarTbc <ripon23105101019@diu.edu.bd>
2026-03-04 14:57:08 +0600

    48_Array_Riduce()

---
+++ 48_Array_Riduce()/Topic_48_Array_Riduce().js
@@ -0,0 +1,41 @@
+//<------Multi line way------>
+const numbers = [1, 2, 3, 4, 5];
+
+const sumResult = numbers.reduce((preValue, currentValue) => {
+    console.log(`preValue: ${preValue}, currentValue: ${currentValue}`);
+    return preValue + currentValue;
+
+  /*'preValue: 0, currentValue: 1' 
+    'preValue: 1, currentValue: 2' 
+    'preValue: 3, currentValue: 3' 
+    'preValue: 6, currentValue: 4' 
+    'preValue: 10, currentValue: 5'
+    return preValue + currentValue;*/
+
+}, 0);
+
+console.log(sumResult); // Output: 15
+
+
+
+
+//<------Single line way------>
+const num = [10, 20, 30, 40, 50];
+
+const result = num.reduce((preValue, currentValue) => preValue + currentValue, 0);
+
+console.log(result); // Output: 150
+
+
+
+
+//<------Shopping card example:------>
+const shoppingCard = [
+    { name: 'item1', price: 100 },
+    { name: 'item2', price: 256 },
+    { name: 'item3', price: 3015 }
+]
+
+const totalPrice = shoppingCard.reduce((preValue, currentValue) => preValue + currentValue.price, 0);
+
+console.log(totalPrice); // Output: 3371
