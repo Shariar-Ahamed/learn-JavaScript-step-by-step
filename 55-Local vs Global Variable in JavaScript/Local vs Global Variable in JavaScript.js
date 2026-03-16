04bd72bb8ba41c087cde5d41a246c6b1d0b29da7
ShahriarTbc <ripon23105101019@diu.edu.bd>
2026-03-16 21:06:31 +0600

    Global and local

---
+++ 55-Local vs Global Variable in JavaScript/Local vs Global Variable in JavaScript.js
@@ -0,0 +1,12 @@
+let b = 'Test text'
+
+function func(){
+    let a = 'Programming learn';
+    console.log(a);      //'Programming learn'
+    console.log(b);     // 'Test text'
+}
+
+func();
+
+console.log(b);      // 'Test text'
+// console.log(a);  // Error
