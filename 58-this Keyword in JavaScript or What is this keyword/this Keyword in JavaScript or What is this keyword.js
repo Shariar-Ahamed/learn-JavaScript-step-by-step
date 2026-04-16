b22896c082e3c37a400df6bbf00fe1dc64856748
ShahriarTbc <ripon23105101019@diu.edu.bd>
2026-04-16 11:55:20 +0600

    this Keyword in JavaScript or What is this keyword

---
+++ 58-this Keyword in JavaScript or What is this keyword/this Keyword in JavaScript or What is this keyword.js
@@ -0,0 +1,25 @@
+/*function test(){
+    console.log(this);
+}
+
+test();*/
+
+// NOTE This is just a note
+
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    job: 'Developer',
+    message: function() {
+        console.log(`Hello! My name is ${this.firstName} and device name is ${product.productName}`);
+    }
+}
+
+const product = {
+    productName: 'iPad',
+    age: 34
+};
+
+
+user.message();  //Output: Hello! My name is John and device name is iPad
