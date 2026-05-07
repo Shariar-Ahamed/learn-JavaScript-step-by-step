3b6e79b7954f028c4d83975e87b257d49e17e33f
ShahriarTbc <ripon23105101019@diu.edu.bd>
2026-05-07 09:08:13 +0600

    Local Storage and Session Storage in javascript

--- 63-Local Storage and Session Storage in javascript/script.js
+++ 63-Local Storage and Session Storage in javascript/script.js
@@ -10,6 +10,8 @@ const setSessionStorage = () => {
     sessionStorage.setItem("email", "[EMAIL_ADDRESS]");
 }
 
+
+
 const removeLocalStorage = () => {
     localStorage.removeItem("name");
     localStorage.removeItem("age");
@@ -20,4 +22,41 @@ const removeSessionStorage = () => {
     sessionStorage.removeItem("name");
     sessionStorage.removeItem("age");
     sessionStorage.removeItem("email");
+}
+
+
+
+
+const getFromLocalStorage = () => {
+    const name = localStorage.getItem("name");
+    const age = localStorage.getItem("age");
+    const email = localStorage.getItem("email");
+    console.log(name, age, email);
+}
+
+const getFromSessionStorage = () => {
+    const name = sessionStorage.getItem("name");
+    const age = sessionStorage.getItem("age");
+    const email = sessionStorage.getItem("email");
+    console.log(name, age, email);
+}
+
+
+
+
+// For clear local storage and session storage we use localStorage.clear() and sessionStorage.clear()
+
+function clearLocalStorage() {
+    localStorage.clear();
+}
+
+function clearSessionStorage() {
+    sessionStorage.clear();
+}
+
+
+// clear all local storage and session storage we use localStorage.clear() and sessionStorage.clear()
+function clearAllLocalStorageAndSessionStorage() {
+    localStorage.clear();
+    sessionStorage.clear();
 }
